7fda4a12e9f3f193533b11b676618969c90a1425
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-10-01 16:48:33 -0500

    finished assert assignment and NaN

--- code.js
+++ code.js
@@ -4,13 +4,41 @@ function helloWorld() {
 }
 
 function sayHello(name){
-    return "Hello, " + name + "!";
+    if(typeof name === "boolean"){
+        return "Hello, World!";
+    } else {
+        return "Hello, " + name + "!";
+    }
 }
 
-function addOne(x){
-    return x + 1;
+function add(x, y){
+    if(typeof x === "number" && typeof y === "number") {
+        return x + y;
+    } else {
+        return "NaN";
+    }
 }
 
 function double(x){
     return x * 2;
+}
+
+function isFive(input){
+    return(input === 5);
+}
+
+function isEven(x){
+    return (x % 2 === 0);
+}
+
+function isVowel(input){
+    if(input === undefined){
+        return false;
+    } else {
+        let check = input.charAt(0);
+        if (check.match(/[aeiou]/i)) {
+            return true;
+        }
+        return false;
+    }
 }
--- tests.js
+++ tests.js
@@ -31,17 +31,83 @@ describe("sayHello", function(){
     it("should return 'Hello, Pat!' when 'Pat' is called", function(){
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    it("should return 'Hello, World!' when true is passed", function(){
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
 });
 
+describe("isFive", function(){
+    it("should return true when passed 5", function(){
+        expect(isFive(5)).toBe(true);
+    });
+    it("should return false when passed '5'", function(){
+        expect(isFive("5")).toBe(false);
+    });
+    it("should return a boolean", function(){
+        expect(typeof isFive()).toBe("boolean");
+    });
+})
 
-describe("addOne", function(){
-    it("should return 2", function (){
-        expect(addOne(1)).toBe(2);
+
+describe("add", function(){
+    it("should return 4 if 1 and 3 are passed through", function (){
+        expect(add(1, 3)).toBe(4);
+    });
+    it("should return NaN if something other than a number is passed through for x and y", function(){
+        expect(add("a", "Y")).toBe("NaN");
+    });
+    it("should return -12 when passed in -3 and -9", function(){
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it("should return NaN if passed 'banana' and 2", function(){
+        expect(add("banana", 2)).toBe("NaN");
     });
 });
 
 describe("double", function(){
     it("should return double the input", function(){
         expect(double(5)).toBe(10);
+    });
+});
+
+describe("isEven", function(){
+    it("should return true when passed 200", function(){
+        expect(isEven(200)).toBe(true);
+    });
+    it("should return false when passed 201", function(){
+        expect(isEven(201)).toBe(false);
+    });
+    it("should return false when passed 'whatAreYouDoingHere'", function (){
+        expect(isEven("whatAreYouDoingHere")).toBe(false);
+    });
+    it("should return true when passed -4", function(){
+        expect(isEven(-4)).toBe(true);
+    });
+    it("should return false when passed 3", function (){
+        expect(isEven(3)).toBe(false);
+    });
+    it("should return false when passed 'banana'", function(){
+        expect(isEven("banana")).toBe(false);
+    });
+});
+
+describe("isVowel", function (){
+    it("should return true when passed 'a'", function(){
+        expect(isVowel("a")).toBe(true);
+    });
+    it("should return false when passed 'k'", function(){
+        expect(isVowel("k")).toBe(false);
+    });
+    it("should return true when passed 'A'", function (){
+        expect(isVowel("A")).toBe(true);
+    });
+    it("should return false when passed 'K'", function (){
+        expect(isVowel("K")).toBe(false);
+    });
+    it("should return false when passed 'banana'", function (){
+        expect(isVowel("banana")).toBe(false);
+    });
+    it("should return false when nothing is passed", function(){
+        expect(isVowel()).toBe(false);
     });
 });
